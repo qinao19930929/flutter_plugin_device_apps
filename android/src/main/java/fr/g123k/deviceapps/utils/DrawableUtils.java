86876dd840b5e3c73f7005d92471dc703a9f0ab1
qinao <1121170319@qq.com>
2021-03-04 10:45:23 +0800

    fix getBitmapFromDrawable bug

--- android/src/main/java/fr/g123k/deviceapps/utils/DrawableUtils.java
+++ android/src/main/java/fr/g123k/deviceapps/utils/DrawableUtils.java
@@ -7,16 +7,30 @@ import android.graphics.drawable.Drawable;
 public class DrawableUtils {
 
     public static Bitmap getBitmapFromDrawable(Drawable drawable) {
-        final Bitmap bmp = Bitmap.createBitmap(
-                drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(),
-                Bitmap.Config.ARGB_8888);
-
-        final Canvas canvas = new Canvas(bmp);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bmp;
+        Bitmap bitmap = null;
+        try {
+            if (drawable instanceof BitmapDrawable) {
+                bitmap = ((BitmapDrawable) drawable).getBitmap();
+            } else {
+                int j = drawable.getIntrinsicWidth();
+                int k = drawable.getIntrinsicHeight();
+                int i = j;
+                if (j <= 0) {
+                    i = 1;
+                }
+                j = k;
+                if (k <= 0) {
+                    j = 1;
+                }
+                bitmap = Bitmap.createBitmap(i, j, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 
 }
